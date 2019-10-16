5beb5d973b4e2aac9c488d9d04a52438bf85ccd8
Harold <harold.malecot@gmail.com>
2019-10-16 15:42:47 +0200

    open CreateMeetingAvtivity when press fab button

---
+++ app/src/main/java/com/example/maru/view/ui/CreateMeetingActivity.java
@@ -0,0 +1,16 @@
+package com.example.maru.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maru.R;
+
+public class CreateMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_meeting);
+    }
+}
--- app/src/main/java/com/example/maru/view/ui/MainActivity.java
+++ app/src/main/java/com/example/maru/view/ui/MainActivity.java
@@ -1,6 +1,9 @@
-package com.example.maru;
+package com.example.maru.view.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.maru.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,14 +21,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                launchCreateMeeting();
             }
         });
+    }
+
+    public void launchCreateMeeting(){
+        Intent intent = new Intent(this.getApplicationContext(), CreateMeetingActivity.class);
+        startActivity(intent);
     }
 
     @Override
