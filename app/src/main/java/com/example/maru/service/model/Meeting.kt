415b86d1cb73c77d21473002956c59fb460192c7
Harold <harold.malecot@gmail.com>
2019-10-16 15:31:17 +0200

    Revert "object Meeting create, start UI of MainActivity, package MVVM created"
    This reverts commit f74516bd

--- app/src/main/java/com/example/maru/service/model/Meeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.maru.service.model
-
-import java.util.*
-
-class Meeting(var date: String = "2019-10-17'T'18:00:00",
-              var place: String = "",
-              var subject: String = "",
-              var listOfEmailOfParticipant: MutableList<String> = Arrays.asList("")) {
-}
