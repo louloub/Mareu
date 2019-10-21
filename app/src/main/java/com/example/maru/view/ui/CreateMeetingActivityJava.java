60db7e2c6240e4ef684535e1b7dfda47746a0730
Harold <harold.malecot@gmail.com>
2019-10-21 14:53:48 +0200

    testing key enter for write new chips

--- app/src/main/java/com/example/maru/view/ui/CreateMeetingActivityJava.java
+++ app/src/main/java/com/example/maru/view/ui/CreateMeetingActivityJava.java
@@ -6,6 +6,7 @@ import android.text.Editable;
 import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.style.ImageSpan;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.Toast;
@@ -23,6 +24,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class CreateMeetingActivityJava extends AppCompatActivity {
 
     private int SpannedLength = 0,chipLength = 4;
+    private boolean keycodeEnterPressed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,23 @@ public class CreateMeetingActivityJava extends AppCompatActivity {
         horizontalScrollView.setBackground(textInputEditText.getBackground());
         textInputEditText.setBackground(null);
         chipsForParticipant(textInputEditText);
+        whenEnterIsCliqued(textInputEditText);
+    }
+
+    public void whenEnterIsCliqued(final TextInputEditText textInputEditText){
+        textInputEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_ENTER:
+                        keycodeEnterPressed = true;
+                        chipsForParticipant(textInputEditText);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     public void chipsForParticipant(final TextInputEditText textInputEditText){
@@ -56,6 +75,29 @@ public class CreateMeetingActivityJava extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
 
+                /*textInputEditText.setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        switch (keyCode) {
+                            case KeyEvent.KEYCODE_ENTER:
+                                keycodeEnterPressed = true;
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });*/
+
+                /*if (editable.length() > 1 && keycodeEnterPressed){
+                    ChipDrawable chip = ChipDrawable.createFromResource(CreateMeetingActivityJava.this, R.xml.chip);
+                    chip.setText(editable.subSequence(SpannedLength,editable.length()-1));
+                    chip.setBounds(0, 0, chip.getIntrinsicWidth(), chip.getIntrinsicHeight());
+                    chip.isCloseIconVisible();
+                    ImageSpan span = new ImageSpan(chip);
+                    editable.setSpan(span, SpannedLength, editable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    SpannedLength = editable.length();
+                }*/
+
                 if (editable.length() > 1 && editable.toString().endsWith(",")){
                     ChipDrawable chip = ChipDrawable.createFromResource(CreateMeetingActivityJava.this, R.xml.chip);
                     chip.setText(editable.subSequence(SpannedLength,editable.length()-1));
@@ -66,13 +108,14 @@ public class CreateMeetingActivityJava extends AppCompatActivity {
                     SpannedLength = editable.length();
                 }
 
-                /*if (editable.length() - SpannedLength == chipLength) {
-                    ChipDrawable chip = ChipDrawable.createFromResource(CreateMeetingActivityJava.this, R.xml.chip);
-                    chip.setText(editable.subSequence(SpannedLength,editable.length()));
-                    chip.setBounds(0, 0, chip.getIntrinsicWidth(), chip.getIntrinsicHeight());
-                    ImageSpan span = new ImageSpan(chip);
-                    editable.setSpan(span, SpannedLength, editable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    SpannedLength = editable.length();
+                /*if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    mButton.performClick();
+                    return true;
+                }
+
+                if (editable.toString().getKeyCode == KeyEvent.KEYCODE_ENTER) {
+                    mButton.performClick();
+                    return true;
                 }*/
             }
         });
