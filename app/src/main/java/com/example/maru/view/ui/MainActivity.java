e80556b6ef08852ce4fb5215edb1ce178ed0dc1c
Harold <harold.malecot@gmail.com>
2019-10-29 14:41:20 +0100

    working on popup for sort room and date

--- app/src/main/java/com/example/maru/view/ui/MainActivity.java
+++ app/src/main/java/com/example/maru/view/ui/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.maru.view.ui;
 
 import android.content.ClipData;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -61,37 +66,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void retriveMeetingFromIntent(){
-        // meeting = (MeetingJava) getIntent().getSerializableExtra("Meeting");
-        // Log.d(TAG, "meeting = " +meeting);
-        // launchRecyclerView();
-    }
-
-    public void launchRecyclerView(){
-        // recyclerView = findViewById(R.id.main_rv);
-        /*recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getApplicationContext(), listMeeting);*/
-
-        // launchRecyclerView(simpleAdapter);
-        // MeetingJava meeting = (MeetingJava) getIntent().getSerializableExtra("Meeting");
-
-        // listMeeting.add(meeting);
-        /*if (meeting != null) {
-            String test = meeting.getSubject();
-            listMeeting.add(meeting);
-            recyclerView.setAdapter(simpleAdapter);
-        } else {
-            Log.d(TAG, "meeting is null ");
-        }*/
-    }
-
+    // Launch intent for create new meeting
     public void launchCreateMeeting() {
         // Intent intent = new Intent(this.getApplicationContext(), CreateMeetingActivity.class);
         Intent intent = new Intent(this.getApplicationContext(), CreateMeetingActivityJava.class);
         startActivity(intent);
     }
 
+    // Button for add meeting
     public void floatingButton() {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -109,12 +91,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // Item selected in toolbar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         if (item.getItemId() == R.id.toolbar_bt_sort_meeting) {
-            // sortData(ascending);
-            if (ascending) {
+            alertDialogChoiceSort();
+            /*if (ascending) {
                 sortData(ascending);
                 Toast toastCrescent = Toast.makeText(MainActivity.this, "Trie croissant", Toast.LENGTH_SHORT);
                 toastCrescent.show();
@@ -124,33 +107,125 @@ public class MainActivity extends AppCompatActivity {
                 Toast toastDecrease = Toast.makeText(MainActivity.this, "Trie décroissant", Toast.LENGTH_SHORT);
                 toastDecrease.show();
                 ascending =! ascending;
-            }
+            }*/
         } else {
             // ELSE
         }
         return super.onOptionsItemSelected(item);
     }
 
+    public void alertDialogChoiceSort(){
+
+        int checkedItems = 0;
+
+        SharedPreferences settings = getSharedPreferences("MySortChoice", Context.MODE_PRIVATE);
+        String sortChoiceSharedPreferences = settings.getString("MySortChoice",null);
+
+        assert sortChoiceSharedPreferences != null;
+
+        if (sortChoiceSharedPreferences==null) {
+            checkedItems = 0;
+        } else {
+
+            if(sortChoiceSharedPreferences.equals("Croissant salle"))
+            { checkedItems = 0; }
+            else if (sortChoiceSharedPreferences.equals("Decroissant salle"))
+            { checkedItems = 1;
+            } else if (sortChoiceSharedPreferences.equals("Croissant date")) {
+                checkedItems = 2;
+            } else if (sortChoiceSharedPreferences.equals("Decroissant date")) {
+                checkedItems = 3;
+            } else { checkedItems = 0; }
+        }
+
+        // Setup Alert builder
+        final AlertDialog.Builder myPopup = new AlertDialog.Builder(this);
+        myPopup.setTitle("Choisis le trie que tu souhaites");
+        // Ddd a radio button list
+        String[] sortChoice = {"Croissant salle", "Decroissant salle", "Croissant date", "Decroissant date"};
+
+        myPopup.setSingleChoiceItems(sortChoice,checkedItems, (new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        }));
+
+        myPopup.setPositiveButton("Valider", (new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ListView lw = ((AlertDialog) dialog).getListView();
+                Object checkedItemObject = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+                Toast.makeText(MainActivity.this.getApplicationContext(), "Tu as choisi " + checkedItemObject, Toast.LENGTH_LONG).show();
+                // finish();
+            }
+        }));
+
+
+        /*myPopup.setSingleChoiceItems(sortChoice, checkedItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AlertDialog.Builder valider = myPopup.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ListView lw = ((AlertDialog) dialog).getListView();
+                        Object checkedItemObject = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+                        Toast.makeText(getApplicationContext(), "Tu as choisi " + checkedItemObject, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });*/
+
+        /*myPopup.setSingleChoiceItems(sortChoice, checkedItems, (dialog, which) -> {
+        });*/
+
+        /*myPopup.setPositiveButton("Valider", (dialogInterface, i) -> {
+            ListView lw = ((AlertDialog)dialogInterface).getListView();
+            Object checkedItemObject = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+            Toast.makeText(getApplicationContext(), "Tu as choisi " + checkedItemObject, Toast.LENGTH_LONG).show();
+            String checkedItemString = checkedItemObject.toString();
+
+            // EDITOR SHARED PREFERENCE
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("MyCityChoice", checkedItemString);
+            editor.apply();
+
+            // FOR RELOAD CONTENT WHEN CITY IS CHOICE
+            finish();
+            startActivity(getIntent());
+        });*/
+
+        /*myPopup.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "Vous avez cliquez sur Non", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        myPopup.setCancelable(false);
+
+        // create and show the alert dialog
+        AlertDialog dialog = myPopup.create();
+        myPopup.show();
+    }
+
     // Comparator to sort meeting list in order of room
     public static Comparator<MeetingJava> MeetingComparator = new Comparator<MeetingJava>() {
-
         @Override
         public int compare(MeetingJava e1, MeetingJava e2) {
             return (e1.getRoom() - e2.getRoom());
         }
     };
 
+    // Sort room
     private void sortData(boolean asc)
     {
         //SORT ARRAY ASCENDING AND DESCENDING
         if (asc)
         {
-            Log.d(TAG, "sortDate trie croissant ");
             Collections.sort(listOfMeeting,MeetingComparator);
         }
         else
         {
-            Log.d(TAG, "sortDate trie decroissant ");
             Collections.reverse(listOfMeeting);
         }
         //ADAPTER
